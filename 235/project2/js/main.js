8c15c136ad01c9fcbf0d6149449a713c47e54285
Robert Reddick (RIT Student) <rdr8959@rit.edu>
2020-11-11 14:09:37 -0500

    Updated general searching

--- 235/project2/js/main.js
+++ 235/project2/js/main.js
@@ -9,16 +9,15 @@ function searchButtonClicked(){
     console.log("searchButtonClicked() called");
     
     // 1
-    const GIPHY_URL = "https://gateway.marvel.com/v1/public/characters?";
+    const MARVEL_URL = "https://gateway.marvel.com/v1/public/characters?";
 
     // 2
-    // Public API key from here: https://developers/giphy.com/docs/
-    // If this one no longer works, get your own (it's free!)
+    // Public API key from here: https://developer.marvel.com/docs/
     let API_KEY = "fedefa1198cc7584d18711f16800dd54";
     let HASH = "00eb4328a410ac808295621f8ff3986a";
 
     // 3 - Build up our URL string
-    let url = GIPHY_URL;
+    let url = MARVEL_URL;
 
     // 4 - parse the user entered term we wish to search
     let term = document.querySelector("#searchterm").value;
@@ -33,14 +32,21 @@ function searchButtonClicked(){
     // 7 - if there's no term to search then bail out of the function (return does this)
     if(term.length < 1) return;
 
-    // 8 - append the search term to the URL - the parameter name is 'q'
-    url += "name=" + term;
-    url += "&apikey=" + API_KEY;
-    url += "&hash=" + HASH;
-    url += "&ts=1";
-
-    // 10 - update the UI
-    //document.querySelector("#status").innerHTML = `<iframe src="https://giphy.com/embed/9rjHo7vXhB9F5by3Wg" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/digitaldjtips-stan-lee-amazing-spider-man-library-dj-9rjHo7vXhB9F5by3Wg">via GIPHY</a></p>`;
+    // Call for different URLs based on the different search types
+    if(!document.querySelector("#searchbybeginswith").checked)
+    {
+        url += "name=" + term;
+        url += "&apikey=" + API_KEY;
+        url += "&hash=" + HASH;
+        url += "&ts=1";
+    }
+    else
+    {
+        url += "nameStartsWith=" + term;
+        url += "&apikey=" + API_KEY;
+        url += "&hash=" + HASH;
+        url += "&ts=1";
+    }
 
     // 11 - see what the URL looks like
     console.log(url);
@@ -83,11 +89,12 @@ function dataLoaded(e)
         return; // Bail out
     }
 
-    // 9 - start building an HTML string we will display to the user
+    // Get the character data
     let result = obj.data;
     console.log(result);
     let charData = result['results'][0];
 
+    // If the character cannot be found show this
     if(charData == null)
     {
         let bigString = "<p><i>Could not find a result for \"" + displayTerm + "\"</i></p>";
@@ -97,6 +104,7 @@ function dataLoaded(e)
     }
     else
     {
+        // If looking for a specific character
         if(!document.querySelector("#searchbybeginswith").checked)
         {
             console.log(charData.name);
@@ -105,11 +113,7 @@ function dataLoaded(e)
             let bigString = "<p><i>Here are the results for \"" + displayTerm + "\"</i></p>";
             let thumbNailString = `<div><img src='${smallURL}' title='${result.id}' /> </div>`
 
-            // 11 - get the URL to the GIF
-            
-            
-            // 13 - Build a <div> to hold each result
-            // ES6 String Templating
+            // If there is no description then change it to none
             if(charData.description != "")
             {
                 let line = `<div><p>Description: ${charData.description}</p></div>`;
@@ -121,11 +125,11 @@ function dataLoaded(e)
                 bigString += line;
             }
 
-            // 16 - all done building the HTML - show it to the user!
+            // Show user
             document.querySelector("#content").innerHTML = bigString;
             document.querySelector("#thumbnail").innerHTML = thumbNailString;
 
-            // 17 - update the status
+            // Update the status
             document.querySelector("#status").innerHTML = "<b>Success!</b>";
 
             // Add any comics the character was in
@@ -168,6 +172,83 @@ function dataLoaded(e)
                 bigString += "</div>";
 
                 document.querySelector("#content").innerHTML += bigString;
+            }
+        }
+        // Looking through a general search
+        else
+        {
+            let chars = result['results'];
+                console.log(chars);
+                
+                let bigString = `<br><p><i>Here is a list of characters under the search "${displayTerm}"</i></p><br><div id="chars">`;
+                for(let i = 0; i < chars.length; i++)
+                {
+                    let char = chars[i]['name'];
+
+
+                    let line = `<button>${char}</button>`;
+
+                    bigString += line;
+
+                }
+                bigString += "</div>";
+
+                document.querySelector("#content").innerHTML = bigString;
+                
+        }
+    }
+
+    // Iterate through each button to check for clicks
+    let buttons = document.querySelectorAll('button');
+    for(let i = 0; i < buttons.length; i++)
+    {
+        // Apply to all buttons except for the search button
+        if(buttons[i].innerHTML != "Find Character!")
+        {
+            // This function basically does the same exact thing as above, 
+            // but now uses the name from the button you clicked 
+            buttons[i].onclick = function(e)
+            {
+                document.querySelector("#searchbybeginswith").checked = false;
+                
+                const MARVEL_URL = "https://gateway.marvel.com/v1/public/characters?";
+
+                let API_KEY = "fedefa1198cc7584d18711f16800dd54";
+                let HASH = "00eb4328a410ac808295621f8ff3986a";
+
+                let url = MARVEL_URL;
+
+                let term = buttons[i].innerHTML;
+                console.log(term);
+                
+                displayTerm = term;
+
+                term = term.trim();
+                
+                term = encodeURIComponent(term);
+
+                if(term.length < 1) return;
+
+                // Call for different URLs based on the different search types
+                if(!document.querySelector("#searchbybeginswith").checked)
+                {
+                    url += "name=" + term;
+                    url += "&apikey=" + API_KEY;
+                    url += "&hash=" + HASH;
+                    url += "&ts=1";
+                }
+                else
+                {
+                    url += "nameStartsWith=" + term;
+                    url += "&apikey=" + API_KEY;
+                    url += "&hash=" + HASH;
+                    url += "&ts=1";
+                }
+
+                console.log(url);
+
+                getData(url);
+                
             }
         }
     }
